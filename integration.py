a44508a074d0a15b2681300cc568463535e01258
Andrew <35786166+classAndrew@users.noreply.github.com>
2020-07-30 16:20:40 -0700

    Update integration.py

--- integration.py
+++ integration.py
@@ -1,6 +1,10 @@
 """
+
+Update: boring, see n_tuple_integral.py
+
 Change a, b, and f to what you want.
 Not very impressive with builtins like sum and range, but still one line!
+
 """
 
 (lambda f, dx, a, b: dx*sum(f(dx*i) for i in range(int(a/dx), int(b/dx))))(lambda x: x**2, 0.0001, 0, 1)
