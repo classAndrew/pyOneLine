7f4d71974c444c9608a4abbbfd9f15e621208413
Andrew <35786166+classAndrew@users.noreply.github.com>
2021-04-18 16:27:57 -0700

    Update n_tuple_integral.py

--- n_tuple_integral.py
+++ n_tuple_integral.py
@@ -1,18 +1,6 @@
 """
-ONE LINER AT THE BOTTOM, here's my explanation.
-This code will compute the definite n-tuple (triple, quadruple, quintuple... n-tuple) integral of some function
-over some bounded region.
-As you can see here, it's a bit short and simple. (Took me FOREVER to debug).
-Basically recursively adds the previous dimension's integral and returns to be added... and so on.
-The demo I have below (in the outermost function's parameters), can be thought of as the 
-"hyper-mass" of a tesseract with edge length 1 whose density is proportional to the distance from the origin.
-The trickiest part is to pass the arguments into the function in the right order (which is why there's
-an awkward unpacking operation going on in the call to f).
-Variable bound can be added, but I just wanted to demo the basic integration over regions with equal sides (1 in this case)
-Slight warning though: THIS DOES NOT SCALE WELL... AT ALL
-In fact, I can't run the below code in under 15 minutes (it will run in O(n^k) time for k is the number of
-variables, and n is the digits of precision, here it is 2 or -log(dx), ON TOP of all the overhead from recursive calls).
-In the one liner, I've swapped out the function to a 3-variable function for my computer's sake.
+Compute Riemann sums of Riemann sums to approximate an iterated integral.
+Bottom line: get ready for you computer fan to explode.
 
 Update: The 4-variable function surprisingly works when ran with pypy (still don't use it though, your computer will fry itself). 
 
